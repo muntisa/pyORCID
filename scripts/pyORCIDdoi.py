4fd79ea5cbcea82fe7d67a8abdfe2bc5f5890fa0
Cristian R Munteanu <muntisa@gmail.com>
2015-01-29 17:29:18 +0100

    Get list of DOIs from an user ORCID

--- scripts/pyORCIDdoi.py
+++ scripts/pyORCIDdoi.py
@@ -1,12 +1,12 @@
-# pyORCIDtex = get ORCID publication as Latex refereces
+# pyORCIDdoi = get DOI of your ORCID publications
 # Author: Cristian R Muntenu (University of A Coruna, Spain)
 # Contact: muntisa@gmail.com
 
 # Usage:
-# python pyORCIDrefs.py "[your ORCID]" > "[outputfile]"
+# python pyORCIDdoi.py "[your ORCID]" > "[outputfile]"
 
 # Example with my ORCID:
-# python pyORCIDrefs.py "0000-0002-5628-2268" > "0000-0002-5628-2268refs.txt"
+# python pyORCIDdoi.py "0000-0002-5628-2268" > "0000-0002-5628-2268doi.txt"
 
 import urllib,sys
 import xml.etree.ElementTree as ET
@@ -18,7 +18,7 @@ def GetORCIDxml(myORCID):
     return
 
 # Convert XML to TEX format of ORCID publication file
-def ORCIDcitations(myORCID):
+def ORCIDdoi(myORCID):
 
     GetORCIDxml(myORCID) # Get XML file with ORCID publications
     
@@ -29,11 +29,20 @@ def ORCIDcitations(myORCID):
     publis = root.findall('.//{http://www.orcid.org/ns/orcid}orcid-work')
     sOut="" # output text string
     for p in publis:
-        # for each publication get the citation
-        pWork_citation= p.find('{http://www.orcid.org/ns/orcid}work-citation')
-        sOut+=(((pWork_citation[1].text).strip()).replace('\n', '')).replace('   ', '')
-        sOut+="\n\n"
-    # print all citations
+        # for each publication get the DOI
+        try:
+            pDOI= p.find('{http://www.orcid.org/ns/orcid}work-external-identifiers')
+            sOut+= pDOI[0][1].text
+        except:
+            # if there is no DOI, get the citation
+            pWork_citation= p.find('{http://www.orcid.org/ns/orcid}work-citation')
+            sOut+=(pWork_citation[1].text).strip()
+            # in addition, get the URL if exists!
+            pURL= p.find('{http://www.orcid.org/ns/orcid}url')
+            if pURL!= None:
+                sOut+= (pURL.text).strip()
+        sOut+="\n"
+    # print the entire results
     print sOut.encode('UTF-8')
     return 
 
@@ -43,6 +52,6 @@ def ORCIDcitations(myORCID):
 
 if __name__ == "__main__":
     print sys.argv[1]
-    ORCIDcitations(sys.argv[1])
+    ORCIDdoi(sys.argv[1])
 
 
